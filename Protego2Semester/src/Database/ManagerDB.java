7d1cb08d0b3040b87101e3db1d8fca0e8d075b7b
jonatxan <jondenlange@icloud.com>
2025-11-23 00:51:05 +0100

    Databasen skulle have sin egen package
    Ny package til database da de adskiller sig fra interface

---
+++ Protego2Semester/src/Database/ManagerDB.java
@@ -0,0 +1,13 @@
+package Database;
+
+public class ManagerDB {
+
+	public static ManagerDB getInstance() {
+		return null;
+}
+
+	public static int getManager(int managerId) {
+		return managerId;
+	}
+
+}
