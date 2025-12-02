1476a172b21de05daa8db97dd42ff153175d212b
Oogwaychild <Englythogersen@gmail.com>
2025-12-02 11:16:00 +0100

    implementing of EmployeeDB & EmployeeDBIF
    har skrevet klassen EmployeeDB og EmployeeDBIF

--- Protego2Semester/src/database/EmployeeDB.java
+++ Protego2Semester/src/database/EmployeeDB.java
@@ -1,8 +1,51 @@
 package database;
+
 import java.sql.*;
 import interfaceDB.EmployeeDBIF;
+import model.Employee;
+import model.Shift;
 
 public class EmployeeDB implements EmployeeDBIF {
+	private static final String CONNECT_SHIFT_TO_EMPLOYEE_Q =
+	"INSERT INTO EmployeeShift (employee_id, shift_id) VALUES (?, ?)"; 
+			
+	
+	private PreparedStatement connectShiftToEmployeestmt;
 
+	private Connection con;
 
+	public EmployeeDB() {
+		try {
+		this.con = DBConnection.getInstance().getConnection();
+		
+		connectShiftToEmployeestmt = con.prepareStatement(CONNECT_SHIFT_TO_EMPLOYEE_Q);
+		}
+		catch (Exception e) {
+		e.printStackTrace();
+		throw new RuntimeException("Could not intialize EmployeeDB", e);
+		}
+	}
+	@Override
+	public void connectShiftToEmployee(Employee employee, Shift shift) {
+	    if (con == null) {
+	        System.out.println("No database connection available");
+	        return;
+	    }
+
+	    try {
+	        connectShiftToEmployeestmt.setInt(1, employee.getEmployeeId());
+	        connectShiftToEmployeestmt.setInt(2, shift.getShiftId());
+	        int rowsAffected = connectShiftToEmployeestmt.executeUpdate();
+
+	        if (rowsAffected > 0) {
+	            System.out.println("Shift assigned successfully: employeeId=" + employee.getEmployeeId() + ", shiftId=" + shift.getShiftId());
+	        } else {
+	            System.out.println("Failed to assign shift to employee.");
+	        }
+
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
 }
--- Protego2Semester/src/interfaceDB/EmployeeDBIF.java
+++ Protego2Semester/src/interfaceDB/EmployeeDBIF.java
@@ -1,5 +1,9 @@
 package interfaceDB;
+import model.Employee;
+import model.Shift;
 
 public interface EmployeeDBIF {
 
+
+	public void connectShiftToEmployee(Employee employee, Shift shift);
 }
