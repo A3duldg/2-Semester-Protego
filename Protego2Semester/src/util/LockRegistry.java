e9d54f4ae7a34f7b3e56d8a9ccca749c91211c4d
Al-Braag <al.braagaard@gmail.com>
2025-12-13 17:58:33 +0100

    // comments to lockRegestry

--- Protego2Semester/src/util/LockRegistry.java
+++ Protego2Semester/src/util/LockRegistry.java
@@ -4,14 +4,20 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public final class LockRegistry {
+	// vi laver et HashMap, hvor Integer er nøgelen(repræsentere et id) og LockEntry er værdien
     private static final ConcurrentHashMap<Integer, LockEntry> map = new ConcurrentHashMap<>();
 
     private static class LockEntry {
         final Object lock = new Object();
+        /* Vi bruger en AtomicInteger der starter på 1 når første tråd anmoder om den
+        Den bruges til at tæller hvor mange der anvender låsen 
+        */
         final AtomicInteger ref = new AtomicInteger(1);
     }
+// Opretter en metode til at hente/oprette en lås for et givent id 
 
     public static Object acquire(int id) {
+    	// opret eller hent LockEntry for id og opdater reference-tæller
         LockEntry existing = map.compute(id, (k, v) -> {
             if (v == null) return new LockEntry();
             v.ref.incrementAndGet();
@@ -20,6 +26,7 @@ public final class LockRegistry {
         return existing.lock;
     }
 
+    // Her har vi metoden for at frigive låsen for et givent id 
     public static void release(int id) {
         map.computeIfPresent(id, (k, v) -> {
             if (v.ref.decrementAndGet() <= 0) return null;
